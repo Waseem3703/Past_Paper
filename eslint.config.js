af56a3b6e58b23554713ccdde0c7e6aa558e0134
[Waseem] <[waseemkhan3703@gmail.com]>
2025-06-22 22:59:27 +0500

    Fix ESLint and TypeScript config for Vercel build

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,22 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+// eslint.config.js
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+  // Ignore files
   { ignores: ['dist'] },
+
+  // JS base rules
+  js.configs.recommended,
+
+  // TypeScript + React rules
+  ...tseslint.configs.recommended,
+
+  // Custom config for TS/TSX files
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -24,5 +33,5 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
     },
-  },
-)
+  }
+);
